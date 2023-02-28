c867a2df668108b7706a87e42bf091771542d7f3
emsitkowski <emsitkowski@gmail.com>
2023-02-28 23:03:07 +0100

    layouts folder added

---
+++ layouts/default.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="container">
+    <Navbar />
+    <Nuxt />
+    <Copyright />
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped></style>
