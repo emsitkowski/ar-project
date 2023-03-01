7c64120769f92b5db7029693a71979c3e16aac60
Michał Sitkowski (x-kom) <emsitkowski@gmail.com>
2023-03-01 15:56:18 +0100

    navbar update

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -11,14 +11,58 @@
         <nuxt-link to="kontakt">Kontakt</nuxt-link>
       </li>
     </ul>
+    <div>
+      <!-- menu icon -->
+      <svg
+        @click="toggleMenu"
+        class="burger"
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        style="fill: rgba(0, 0, 0, 1); transform: ; msfilter: "
+      >
+        <path d="M4 6h16v2H4zm0 5h16v2H4zm0 5h16v2H4z"></path>
+      </svg>
+    </div>
     <div class="logo">
       <img src="~/assets/img/ar-logo.svg" alt="" />
+    </div>
+
+    <!-- fullscreen menu -->
+    <div v-if="isMenuOpen" class="menu">
+      <svg
+        @click="toggleMenu"
+        class="menu__close"
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        style="fill: rgba(0, 0, 0, 1); transform: ; msfilter: "
+      >
+        <path
+          d="m16.192 6.344-4.243 4.242-4.242-4.242-1.414 1.414L10.535 12l-4.242 4.242 1.414 1.414 4.242-4.242 4.243 4.242 1.414-1.414L13.364 12l4.242-4.242z"
+        ></path>
+      </svg>
+
+      <div class="menu__content">text</div>
     </div>
   </nav>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      isMenuOpen: true,
+    };
+  },
+  methods: {
+    toggleMenu() {
+      this.isMenuOpen = !this.isMenuOpen;
+    },
+  },
+};
 </script>
 
 <style lang="scss">
@@ -27,18 +71,28 @@ export default {};
   transition: 0.2s linear;
 }
 nav {
+  position: relative;
   display: flex;
   align-items: center;
+  justify-content: center;
   margin-top: 32px;
   margin-bottom: 86px;
+  @media (min-width: 768px) {
+    justify-content: flex-start;
+  }
+
   ul {
-    display: flex;
+    display: none;
     align-items: center;
     gap: 20px;
     list-style-type: none;
     width: 33.333%;
+    @media (min-width: 768px) {
+      display: flex;
+    }
 
     li {
+      white-space: nowrap;
       a {
         color: #999;
         text-decoration: none;
@@ -53,6 +107,48 @@ nav {
     text-align: center;
     img {
       width: 42px;
+    }
+  }
+
+  .burger {
+    position: absolute;
+    top: 50%;
+    right: 0;
+    transform: translateY(-50%);
+    user-select: none;
+    cursor: pointer;
+    @media (min-width: 768px) {
+      display: none;
+    }
+  }
+
+  .menu {
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+    width: 100%;
+    height: 100%;
+    background-color: #fff;
+    z-index: 100;
+    @media (min-width: 768px) {
+      display: none;
+    }
+
+    &__content {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      width: 100%;
+      height: 100%;
+    }
+
+    &__close {
+      position: absolute;
+      right: 6%;
+      top: 38px;
+      user-select: none;
+      cursor: pointer;
     }
   }
 }
