5656a8f334a968e34ad2e4b414316f98154154a3
emsitkowski <emsitkowski@gmail.com>
2023-03-10 13:45:48 +0100

    Change title

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: "ar-project",
+    title: "Agnieszka Radkiewicz – portfolio",
     htmlAttrs: {
       lang: "pl",
     },
