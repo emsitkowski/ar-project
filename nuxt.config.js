65e5985916a6a30b4bccdbf6a9552e7cd7be25c1
emsitkowski <emsitkowski@gmail.com>
2023-03-10 13:53:29 +0100

    Add favicons

--- nuxt.config.js
+++ nuxt.config.js
@@ -19,7 +19,24 @@ export default {
       },
     ],
     link: [
-      { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
+      {
+        rel: "icon",
+        type: "image/png",
+        sizes: "32x32",
+        href: "/favicon-32x32.png",
+      },
+      {
+        rel: "icon",
+        type: "image/png",
+        sizes: "192x192",
+        href: "/favicon-192x192.png",
+      },
+      {
+        rel: "apple-touch-icon",
+        sizes: "180x180",
+        href: "/apple-touch-icon.png",
+      },
+
       { rel: "preconnect", href: "https://fonts.googleapis.com" },
       {
         rel: "preconnect",
