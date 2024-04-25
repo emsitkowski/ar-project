59c0a63d8411bc2c7e95263438b9743c4b728492
Michał Sitkowski (x-kom) <emsitkowski@gmail.com>
2024-04-25 09:33:16 +0200

    Remove api access key

--- composables/fetchSingleProject.js
+++ composables/fetchSingleProject.js
@@ -4,11 +4,9 @@ import { getCurrentRef } from "../composables/getCurrentRef";
 export async function fetchSingleProject({ params }) {
   // get current API ref
   const ref = await getCurrentRef();
-  const access_token =
-    "MC5aQWVsN3hBQUFDZ0FVNlJw.77-977-977-9bXrvv73vv73vv73vv73vv71-77-977-9Tu-_vSAjHgp5JVnvv73vv70Y77-977-977-977-977-977-9QA";
 
   return await fetch(
-    `https://agaradkiewicz.cdn.prismic.io/api/v2/documents/search?ref=${ref}&access_token=${access_token}`
+    `https://agaradkiewicz.cdn.prismic.io/api/v2/documents/search?ref=${ref}`
   )
     .then((res) => res.json())
     .then(
--- composables/getCurrentRef.js
+++ composables/getCurrentRef.js
@@ -1,11 +1,6 @@
 // >>>> Get current API ref
 export async function getCurrentRef() {
-  const access_token =
-    "MC5aQWVsN3hBQUFDZ0FVNlJw.77-977-977-9bXrvv73vv73vv73vv73vv71-77-977-9Tu-_vSAjHgp5JVnvv73vv70Y77-977-977-977-977-977-9QA";
-
-  const ref = await fetch(
-    `https://agaradkiewicz.cdn.prismic.io/api/v2?access_token=${access_token}`
-  )
+  const ref = await fetch(`https://agaradkiewicz.cdn.prismic.io/api/v2`)
     .then((res) => res.json())
     .then((res) => res.refs[0].ref);
   return ref;
