f7fcfe45743e51985aa2f5617d1ae5701b45f690
Michał Sitkowski <emsitkowski@gmail.com>
2024-07-28 19:11:24 +0200

    feat: add developer info to footer

--- components/Copyright.vue
+++ components/Copyright.vue
@@ -1,6 +1,12 @@
 <template>
   <div class="copyright">
     <p>Copyright by Aga Radkiewicz</p>
+    <p>
+      Wykonanie strony:
+      <a href="https://emsitkowski.com" target="_blank" rel="noopener"
+        ><strong>emsitkowski.com</strong></a
+      >
+    </p>
   </div>
 </template>
 
@@ -10,6 +16,9 @@ export default {};
 
 <style lang="scss" scoped>
 .copyright {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
   padding: 64px 0;
   text-align: center;
 
@@ -20,6 +29,17 @@ export default {};
   p {
     color: #999;
     font-size: 14px;
+  }
+
+  a {
+    color: black;
+    &:hover {
+      text-decoration: underline;
+    }
+
+    strong {
+      font-weight: 500;
+    }
   }
 }
 </style>
